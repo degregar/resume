8cf596c217002489fe666b9996be6801ad7b9a7a
Michał Kukla <degregar@gmail.com>
2022-09-30 17:55:11 +0200

    Update tech stack

--- src/components/Resume/SidebarBoxes/SidebarBoxes.tsx
+++ src/components/Resume/SidebarBoxes/SidebarBoxes.tsx
@@ -1,16 +1,16 @@
-import React, { useContext } from "react";
-import styled from "styled-components";
+import React, { useContext } from "react"
+import styled from "styled-components"
 
-import DetailsContext, { DETAILS_MODES } from "../../../context/DetailsContext";
+import DetailsContext, { DETAILS_MODES } from "../../../context/DetailsContext"
 
-import phoneIcon from "../../../assets/icons/mobile.svg";
-import emailIcon from "../../../assets/icons/email.svg";
-import addressIcon from "../../../assets/icons/address.svg";
+import phoneIcon from "../../../assets/icons/mobile.svg"
+import emailIcon from "../../../assets/icons/email.svg"
+import addressIcon from "../../../assets/icons/address.svg"
 
-import facebookIcon from "../../../assets/icons/facebook.svg";
-import linkedinIcon from "../../../assets/icons/linkedin.svg";
-import githubIcon from "../../../assets/icons/github.svg";
-import youtubeIcon from "../../../assets/icons/youtube.svg";
+import facebookIcon from "../../../assets/icons/facebook.svg"
+import linkedinIcon from "../../../assets/icons/linkedin.svg"
+import githubIcon from "../../../assets/icons/github.svg"
+import youtubeIcon from "../../../assets/icons/youtube.svg"
 
 export const ContactBox = () => (
   <Box>
@@ -34,7 +34,7 @@ export const ContactBox = () => (
       </li>
     </List>
   </Box>
-);
+)
 
 export const SocialMediaBox = () => {
   return (
@@ -42,75 +42,68 @@ export const SocialMediaBox = () => {
       <h2 className="heading">Social Media</h2>
       <List>
         <li>
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href="https://www.linkedin.com/in/kukla-michal/"
-          >
+          <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/kukla-michal/">
             <Icon alt="LinkedIn" src={linkedinIcon} />
             <span>LinkedIn</span>
           </a>
         </li>
         <li>
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href="https://www.youtube.com/channel/UCUDCuwRHCLa5M70Du5YM_uQ/videos?view_as=subscriber"
-          >
+          <a rel="noreferrer" target="_blank" href="https://www.youtube.com/channel/UCUDCuwRHCLa5M70Du5YM_uQ/videos?view_as=subscriber">
             <Icon alt="YouTube" src={youtubeIcon} />
             <span>YouTube</span>
           </a>
         </li>
         <li>
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href="https://github.com/degregar/resume"
-          >
+          <a rel="noreferrer" target="_blank" href="https://github.com/degregar/resume">
             <Icon alt="GitHub" src={githubIcon} />
             <span>GitHub</span>
           </a>
         </li>
         <li>
-          <a
-            rel="noreferrer"
-            target="_blank"
-            href="https://facebook.com/degregar"
-          >
+          <a rel="noreferrer" target="_blank" href="https://facebook.com/degregar">
             <Icon alt="Facebook" src={facebookIcon} />
             <span>Facebook</span>
           </a>
         </li>
       </List>
     </Box>
-  );
-};
+  )
+}
 
 export const StackBox = () => {
   return (
     <Box>
       <h2 className="heading">Tech Stack</h2>
-      <p>Scale: intern - junior - mid - senior - expert</p>
+
+      <p style={{ color: "#aaa" }}>Used scale: intern - junior - mid - senior - expert</p>
+
       <List>
         <li>
+          <span className="label">Senior</span>
+          <span>TypeScript, NodeJS, React, Vanilla JS, ES6</span>
+        </li>
+        <li>
           <span className="label">Mid / Senior</span>
-          <span>React, Vanilla JS, ES6, CSS3, HTML5, Bootstrap4, PHP, MySQL, jQuery, WordPress</span>
+          <span>CSS3, HTML5, Bootstrap4, PHP, MySQL, jQuery, WordPress, REST, Git, Docker, TDD, Adobe Illustrator Scripting, MobX</span>
         </li>
         <li>
           <span className="label">Junior / Mid</span>
-          <span>Gatsby, Sass, TailwindCSS, Webpack, Redux, Git, Jira, Firebase, OOCSS, BEM, Docker, TDD, CI/CD, Scrum, UX, GraphQL, Figma, GSAP</span>
+          <span>
+            AWS (S3, SQS, Cloudfront, EC2, Terraform), NestJS, NextJS, Python, Gatsby, Sass, TailwindCSS, Webpack, Redux, Jira, Firebase (storage,
+            queues, functions), OOCSS, BEM, CI/CD, Scrum, UX, GraphQL, Figma, GSAP, Babel, Storybook, Datadog, Airflow, Spark
+          </span>
         </li>
         <li>
           <span className="label">Junior</span>
-          <span>Storybook, NodeJS, JS Service Workers, Babel, TypeScript, PWA, Java, Python, ML</span>
+          <span>JS Service Workers, PWA, Java, ML, Rust, Ruby</span>
         </li>
       </List>
     </Box>
-  );
-};
+  )
+}
 
 export const LanguagesBox = () => {
-  const { mode: detailsMode } = useContext(DetailsContext);
+  const { mode: detailsMode } = useContext(DetailsContext)
 
   return (
     <Box>
@@ -132,14 +125,14 @@ export const LanguagesBox = () => {
         )}
       </List>
     </Box>
-  );
-};
+  )
+}
 
-const Box = styled.div``;
+const Box = styled.div``
 
 const Icon = styled.img`
   width: 24px;
-`;
+`
 
 const List = styled.ul`
   list-style: none;
@@ -166,4 +159,4 @@ const List = styled.ul`
     width: 110px;
     flex-shrink: 0;
   }
-`;
+`
