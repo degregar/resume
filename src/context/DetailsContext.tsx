c4d0d4a015ee2b3b7e4e710c9cc4d3e5f0f4ee24
Michał Kukla <degregar@gmail.com>
2023-12-11 09:50:06 +0100

    fix: show standard resume version by default instead of full version

--- src/context/DetailsContext.tsx
+++ src/context/DetailsContext.tsx
@@ -12,9 +12,9 @@ type DetailsContextType = {
   setMode: (mode: string) => void
 }
 
-export const DetailsContext = React.createContext<DetailsContextType>({ mode: DETAILS_MODES.full, setMode: () => {} })
+export const DetailsContext = React.createContext<DetailsContextType>({ mode: DETAILS_MODES.standard, setMode: () => {} })
 
-const DetailsContextProvider = ({ mode: newMode = DETAILS_MODES.full, ...params }) => {
+const DetailsContextProvider = ({ mode: newMode = DETAILS_MODES.standard, ...params }) => {
   const [mode, setMode] = useState(newMode)
 
   return (
