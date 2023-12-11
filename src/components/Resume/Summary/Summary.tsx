d27bec7c1a4c8c6a2512672d91a91376a714ee68
Michał Kukla <degregar@gmail.com>
2023-12-11 09:49:42 +0100

    feat: add microskills experience

--- src/components/Resume/Summary/Summary.tsx
+++ src/components/Resume/Summary/Summary.tsx
@@ -32,6 +32,12 @@ export const Summary = () => {
       summary = null
   }
 
+  const microskillsExperience = (
+    <li>
+      <strong>MicroSkills.pl</strong> - web application for professionals to answer questions like: what skills do I have, what skills should I learn right now to meet my professional goals quickly. App written in Next.js + Neo4j
+    </li>
+  )
+
   const waasExperience = (
     <li>
       <strong>WaaS</strong> - Website as a Service for restaurants, written in Gatsby / React, backend in WordPress - he was managing work of his two
@@ -71,18 +77,23 @@ export const Summary = () => {
         <>
           {programmerExperience}
 
-          <p>
-            His first experience with computers was when he were 8 years old. He got a computer as First Communion gift. He's started writing first
-            websites in the age of 17.
-          </p>
+          {detailsMode === DETAILS_MODES.full && (
+          <>
+              <p>
+                    His first experience with computers was when he were 8 years old. He got a computer as First Communion gift. He's started writing first
+                    websites in the age of 17.
+              </p>
+              <p>
+                  He always tells a story about how he was sitting in fear for two and a half hours, downloading his first, 12MB local PHP server, using
+                  dial-up modem. He was afraid of two things: interrupting the connection and a look on his father's face, while he's reading the bill.
+              </p>
+          </>
+          )}
 
-          <p>
-            He always tells a story about how he was sitting in fear for two and a half hours, downloading his first, 12MB local PHP server, using
-            dial-up modem. He was afraid of two things: interrupting the connection and a look on his father's face, while he's reading the bill.
-          </p>
 
           <p>In his work he’s created tools like:</p>
           <ul>
+            {microskillsExperience}
             {waasExperience}
             {kuklaTechExperience}
             {meetforgeExperience}
@@ -95,7 +106,7 @@ export const Summary = () => {
             <li>
               <p>
                 <span>Team leader's perspective</span>
-                {detailsMode === DETAILS_MODES.full && (
+                {[DETAILS_MODES.full, DETAILS_MODES.standard].includes(detailsMode) && (
                   <span>
                     , because he has 6-years experience in managing work of 3-12 people team in technology, travel and marketing industries.
                   </span>
@@ -106,7 +117,7 @@ export const Summary = () => {
             <li>
               <p>
                 <span>Client's perspective</span>
-                {detailsMode === DETAILS_MODES.full && (
+                {[DETAILS_MODES.full, DETAILS_MODES.standard].includes(detailsMode) && (
                   <span>
                     , because he's worked in marketing and learned UX/UI. He's used it in his own applications, while creating web projects in
                     WordPress for digital and creative agencies and for individuals.
@@ -118,7 +129,7 @@ export const Summary = () => {
             <li>
               <p>
                 <span>Entrepreneur's perspective</span>
-                {detailsMode === DETAILS_MODES.full && (
+                {[DETAILS_MODES.full, DETAILS_MODES.standard].includes(detailsMode) && (
                   <span>
                     , as he's started several companies, he's run a 3-star hotel and diet catering for 2 years. He's been networking once a week for
                     almost a year with a group of dozens of entrepreneurs.
@@ -129,6 +140,8 @@ export const Summary = () => {
           </ol>
         </>
       )}
+
+        <p>Continuously learns how to be a better person, programmer, entrepreneur, leader and trainer. How? By reading <a href={"https://michalkukla.pl/polecane-ksiazki/"} target={"_blank"}>tons of books</a> and taking courses like DNA, Legacy Fighter, AnF (devstyle), MBLC (Mindfulness Association), online trainers (Grupa SET).</p>
     </div>
   )
 }
